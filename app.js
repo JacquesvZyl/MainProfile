94766ad7efc534bb140121362b96c0e6f3e02608
jacquesvzyl <jacquesf.vanzyl@gmail.com>
2022-02-28 13:53:37 +0700

    Currently refactoring code in a OOP fashion

--- app.js
+++ app.js
@@ -27,6 +27,244 @@ const skillsHtml = `
 <p>Unity</p>
 </div>`;
 
+/// Custom Class ///
+class App {
+  #html;
+  #popup;
+  constructor() {
+    container.addEventListener("click", this._newPopup.bind(this));
+    container.addEventListener("click", this._toggleTaskbarMinimize.bind(this));
+    container.addEventListener("click", this._togglePopupMinimize.bind(this));
+    container.addEventListener("click", this._closePopup.bind(this));
+    this._setTime();
+    this._removeClippy();
+  }
+
+  // Remove clippy after animation
+  _removeClippy() {
+    setTimeout(() => {
+      document.querySelector(".clippy__container").classList.add("hidden");
+    }, 10000);
+  }
+  //Set taskbar time
+  _setTime() {
+    const options = {
+      hour: "numeric",
+      minute: "numeric",
+    };
+    time.innerText = new Date().toLocaleTimeString("en-GB", options);
+
+    setInterval(() => {
+      time.innerText = new Date().toLocaleTimeString("en-GB", options);
+    }, 1000);
+  }
+
+  // Toggle hidden class
+  _toggleHiddenClass(element) {
+    element.classList.toggle("hidden");
+  }
+
+  _togglePopupMinimizeClass(element) {
+    element.classList.toggle("minimized");
+  }
+
+  _newPopup(e) {
+    if (e.target.closest(".shortcut")) {
+      const eventTarget = e.target.closest(".shortcut");
+      const title = eventTarget.querySelector(".shortcut__title");
+      const icon = eventTarget.querySelector(".shortcut__icon");
+
+      if (eventTarget.classList.contains("shortcut__skills")) {
+        this.#popup = ".skills__popup";
+        this.#html = `<div class="skills__popup draggable popup win95__border">
+  <div class="basic__window__top__container">
+      <div class="basic__window_top">
+          <div class="basic__window__top__left">
+              <img class="popup__icon" src="${icon.src}" alt="">
+              <div class="popup__title">${title.innerText}</div>
+          </div>
+          <div class="basic__window__top__right">
+              <div class="minimize__popup__button popup__button win95__border">
+                  <div class="minimize__box"></div>
+              </div>
+              <div class="close__popup__button popup__button win95__border">&#10006</div>
+          </div>
+      </div>
+  </div>
+
+  <div class="popup__main__section__container">
+      <div class="popup__main_section">
+          <div class="popup__main__text">
+              <p>Microsoft(R) Windows 95</p>
+              <p>(C)Copyright Microsoft Corp 1981-1996.</p>
+              <p class="skills__cd">C:\\Users\\Jacques></p>
+              <p class="skills__dir"></p>
+              <div class="skills__list">
+
+              </div>
+              <p class='skills__end'></p>
+          </div>
+      </div>
+  </div>
+</div>`;
+      }
+      if (eventTarget.classList.contains("shortcut__help")) {
+        this.#popup = ".help__popup";
+        this.#html = `<div class="help__popup draggable popup win95__border">
+        <div class="basic__window__top__container">
+            <div class="basic__window_top">
+                <div class="basic__window__top__left">
+                    <img class="popup__icon" src="${icon.src}" alt="">
+                    <div class="popup__title">${title.innerText}</div>
+                </div>
+                <div class="basic__window__top__right">
+                    <div class="minimize__popup__button popup__button win95__border">
+                        <div class="minimize__box"></div>
+                    </div>
+                    <div class="close__popup__button popup__button win95__border">&#10006</div>
+                </div>
+            </div>
+            <div class="basic__window__middle">
+                <span>File</span>
+                <span>Edit</span>
+                <span>View</span>
+                <span>Help</span>
+            </div>
+        </div>
+      
+        <div class="popup__main__section__container">
+            <div class="popup__main_section">
+                <div class="popup__main__text">
+                    <h1>Hi! I'm Jacques</h1>
+                    <p>I'm a 36 year old self-taught front-end developer. Originally from Cape Town, South Africa,
+                        I'm
+                        currently living in Bangkok,
+                        Thailand with my wonderful wife since late 2020.</p>
+                    <p>In 2020 I was retrenched from my job as an Application support engineer. I thought to myself
+                        that this was the perfect opportunity to change my career path to something that I actually
+                        enjoy...development!</p>
+      
+                    <p>I have since taught myself the Unity engine and C# for game Development, mostly for fun and
+                        out of curiosity.</p>
+                    <p>One of my android games was actually <a target="_blank"
+                            href="https://www.pocketgamer.com/golf-in-60-seconds/">nominated for best sport game of
+                            2021 on PocketGamer.com.</a> </p>
+                    <p>I'm currently laser focused on web development and taught myself HTML, CSS, JavaScript,
+                        Nodejs with express, and mongoDB.</p>
+                    <p>I'm fascinated with creating content and user experiences with code.</p>
+                </div>
+                <div class="popup__main__image">
+                    <img src="./src/images/other_images/Jacques.jpg" alt="">
+                </div>
+            </div>
+        </div>
+        <div class="basic__window__bottom__container">
+            <div class="basic__window__bottom_left win95__border__invert">1 object(s)</div>
+            <div class="basic__window__bottom_rest win95__border__invert">
+                <div class="basic__window__bottom__rest__text">352 bytes</div>
+                <img src="/src/images/win95_icons/3lines.png" alt="">
+            </div>
+        </div>
+      </div>`;
+      }
+      if (eventTarget.classList.contains("shortcut__contact")) {
+        this.#popup = ".email__popup";
+        this.#html = `<div class="email__popup draggable popup win95__border">
+        <div class="basic__window__top__container">
+            <div class="basic__window_top">
+                <div class="basic__window__top__left">
+                    <img class="popup__icon" src="${icon.src}" alt="">
+                    <div class="popup__title">${title.innerText}</div>
+                </div>
+                <div class="basic__window__top__right">
+                    <div class="minimize__popup__button popup__button win95__border">
+                        <div class="minimize__box"></div>
+                    </div>
+                    <div class="close__popup__button popup__button win95__border">&#10006</div>
+                </div>
+            </div>
+        </div>
+        <form id="contact__form" action="https://formspree.io/f/xoqrdpbb" method="POST">
+            <div class="email__button__container">
+                <button id="form__submit" type="submit">
+                    <img src="./src/images/win95_icons/Letter.ico" alt="">
+                    <span>Send</span>
+                </button>
+                <div id="status"></div>
+            </div>
+            <div class="email__inputs">
+                <h2>New Message</h2>
+                <p>To: <span>Jacques</span> </p>
+                <div>
+                    <label for="email">From:</label>
+                    <input type="email" autocomplete="off" id="email" name="email">
+                </div>
+                <div>
+                    <label for="subject">Subject:</label>
+                    <input type="text" autocomplete="off" id="subject" name="subject">
+                </div>
+      
+                <textarea name="message" id="message" style="resize: none;"></textarea>
+            </div>
+        </form>
+      </div>`;
+      }
+      //only create popup if it doesnt currently exist
+      if (!document.querySelector(this.#popup)) {
+        container.insertAdjacentHTML("beforeend", this.#html);
+        this._addToTaskbar(this.#popup, icon, title);
+      }
+    }
+  }
+
+  _addToTaskbar(popupName, icon, title) {
+    const popup = document.querySelector(popupName);
+    console.dir(popup);
+    // Add to taskbar id popup if visible
+    if (popup && !popup.classList.contains("hidden")) {
+      //prettier-ignore
+      const html = `<div class="taskbar__popup win95__border__clicked close" data-parent="${popup.classList[0]}">
+    <img src=${icon.src} alt="">
+    <span>${textTruncate(title.innerText, 20)}</span>
+  </div>`;
+      startMenuBtn.insertAdjacentHTML("afterend", html);
+    }
+  }
+
+  _toggleTaskbarMinimize(e) {
+    if (e.target.closest(".taskbar__popup")) {
+      const currentTaskbar = e.target.closest(".taskbar__popup");
+      const popup = currentTaskbar.dataset.parent;
+      const popupElement = document.querySelector(`.${popup}`);
+      this._toggleHiddenClass(popupElement);
+      this._togglePopupMinimizeClass(currentTaskbar);
+    }
+  }
+
+  _togglePopupMinimize(e) {
+    if (e.target.closest(".minimize__popup__button")) {
+      const popup = e.target.closest(".popup");
+      //prettier-ignore
+      const taskbarShortcut = taskbar.querySelector(`[data-parent='${popup.classList[0]}'`);
+      console.log(popup.classList);
+      this._togglePopupMinimizeClass(taskbarShortcut);
+      this._toggleHiddenClass(popup);
+    }
+  }
+
+  _closePopup(e) {
+    if (e.target.closest(".close__popup__button")) {
+      const popup = e.target.closest(".popup");
+      //prettier-ignore
+      const taskbarShortcut = taskbar.querySelector(`[data-parent='${popup.classList[0]}'`);
+      popup.remove();
+      taskbarShortcut.remove();
+    }
+  }
+}
+
+const app = new App();
+
 ////// 3. FUNCTIONS /////
 
 // 3.1 Random Functions //
@@ -93,24 +331,6 @@ async function typeWriter(txt, element, waitTime, typeSpeed) {
   type();
 }
 
-// Generate time in taskbar
-function setTime() {
-  const options = {
-    hour: "numeric",
-    minute: "numeric",
-  };
-  time.innerText = new Date().toLocaleTimeString("en-GB", options);
-
-  setInterval(() => {
-    time.innerText = new Date().toLocaleTimeString("en-GB", options);
-  }, 1000);
-}
-
-// Toggle hidden class
-function toggleHiddenClass(element) {
-  element.classList.toggle("hidden");
-}
-
 // truncate Long strings
 function textTruncate(str, length, ending) {
   if (length == null) {
@@ -136,7 +356,6 @@ function setDraggableClass(eventTarget) {
       eventTarget.closest(".draggable") === item &&
       !elementTypes.includes(eventTarget.localName)
     ) {
-      console.dir(eventTarget);
       item.classList.add("being__dragged");
       return item;
     }
@@ -198,34 +417,6 @@ function setTranslate(xPos, yPos, el) {
 }
 
 // 3.3 ADD CURRENT OPEN APP OR DOC TO TASKBAR //
-
-function addRemoveToTaskbar(popup) {
-  // Add to taskbar id popup if visible
-  if (popup && !popup.classList.contains("hidden")) {
-    const icon = popup.querySelector(".popup__icon");
-    const title = popup.querySelector(".popup__title");
-    const html = `<div class="taskbar__popup win95__border__clicked close" data-parent-class="${
-      popup.classList[0]
-    }">
-  <img src=${icon.src} alt="">
-  <span>${textTruncate(title.innerText, 20)}</span>
-</div>`;
-    startMenuBtn.insertAdjacentHTML("afterend", html);
-    const currentItem = taskbar.querySelector(
-      `[data-parent-class='${popup.classList[0]}'`
-    );
-    currentItem.addEventListener("click", () => {
-      toggleHiddenClass(popup);
-      currentItem.classList.toggle("minimized");
-    });
-  } else {
-    // Delete from taskbar if popup is hidden or removed
-    const popupTaskbar = taskbar.querySelector(
-      `[data-parent-class='${popup.classList[0]}'`
-    );
-    popupTaskbar.remove();
-  }
-}
 
 // 3.4 POPUP FUNCTIONS
 function whoAmiPopup(e) {
@@ -378,7 +569,7 @@ function contactFormPopup(e) {
 
 //// 4. EVENT LISTENERS & FUNCTION EXECUTION //////
 
-container.addEventListener("click", (e) => {
+/* container.addEventListener("click", (e) => {
   // SETTING UP START BTN EVENT LISTENER;
   if (e.target.closest(".start__btn")) {
     const currentTarget = e.target.closest(".start__btn");
@@ -507,16 +698,8 @@ container.addEventListener("click", (e) => {
       }
     }
   }
-});
+}); */
 
 container.addEventListener("mousedown", dragStart);
 container.addEventListener("mouseup", dragEnd);
 container.addEventListener("mousemove", drag);
-
-// sets clippy container to hidden after animation has played
-setTimeout(() => {
-  document.querySelector(".clippy__container").classList.add("hidden");
-}, 10000);
-
-// set Taskbar clock
-setTime();
